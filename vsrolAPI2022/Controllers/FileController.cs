14cea07286847ae7298d45c82e5bc2b353709488
TruongNghia <nguyentruongnghiacntt@gmail.com>
2024-06-27 14:10:57 +0700

    fix code

---
+++ vsrolAPI2022/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VS.Core.Business.Interface;
+namespace vsrolAPI2022.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController
+    {
+        private readonly IFileBussiness _business;
+        public FileController(IFileBussiness autoBussiness)
+
+        {
+            _business = autoBussiness;
+        }
+
+        [HttpGet("~/api/file/getFileResult")]
+        public async Task<IResult> GetFileResult()
+        {
+            await _business.GetFileResult();
+            return Results.Ok(new { success = true });
+        }
+
+    }
+}
